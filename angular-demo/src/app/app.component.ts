3438ac36a74bd5fdb16247b078a151886a40310a
poetries <jingguanliuye@gmail.com>
2019-01-08 14:48:25 +0800

    update

---
+++ angular-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root', // 使用这个组件的名称
+  templateUrl: './app.component.html', // html
+  styleUrls: ['./app.component.scss'] // css
+})
+export class AppComponent {
+  title = 'My First Angular App!';
+}
---
+++ angular-demo/src/app/app.module.ts
@@ -0,0 +1,29 @@
+// 浏览器解析模块
+import { BrowserModule } from '@angular/platform-browser';
+
+// 核心模块
+import { NgModule } from '@angular/core';
+
+// 根路由
+import { AppRoutingModule } from './app-routing.module';
+
+// 跟组件
+import { AppComponent } from './app.component';
+
+// AppModule装饰器 告诉angular如何启动和编译应用
+@NgModule({
+  declarations: [ // 配置当前项目运行组件
+    AppComponent
+  ],
+  imports: [ // 配置当前模块运行依赖的模块
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [], // 配置项目所需服务
+
+  // 生明模块的根组件
+  bootstrap: [AppComponent]
+})
+
+//  根模块不需要导出任何东西 因为其他模块不需要导入根模块
+export class AppModule { }
