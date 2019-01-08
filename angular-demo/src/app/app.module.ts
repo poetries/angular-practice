3f971f2c90a115dd80078d3e6813d51ed33ae4d7
poetries <jingguanliuye@gmail.com>
2019-01-08 19:32:19 +0800

    update

--- angular-demo/src/app/app.module.ts
+++ angular-demo/src/app/app.module.ts
@@ -13,20 +13,27 @@ import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { SearchComponent } from './components/search/search.component';
+
+// 引入服务
+import { StorageService } from './services/storage.service';
 
 // AppModule装饰器 告诉angular如何启动和编译应用
 @NgModule({
   declarations: [ // 配置当前项目运行组件
     AppComponent,
      HomeComponent,
-     FormComponent
+     FormComponent,
+     TodoComponent,
+     SearchComponent
   ],
   imports: [ // 配置当前模块运行依赖的模块
     BrowserModule,
     AppRoutingModule,
     FormsModule
   ],
-  providers: [], // 配置项目所需服务
+  providers: [StorageService], // 配置项目所需服务
 
   // 生明模块的根组件
   bootstrap: [AppComponent]
--- angular-demo/src/app/components/form/form.component.ts
+++ angular-demo/src/app/components/form/form.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  public peopleInfo = {
+
+    username: '',
+    sex: '2',
+    cityList: ['北京', '上海', '深圳'],
+    city: '上海',
+
+    hobby:[{
+          title: '吃饭',
+          checked:false
+      },{
+
+            title:'睡觉',
+            checked:false
+        },{
+
+          title:'敲代码',
+          checked:true
+      }],
+
+      mark:''
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+  doSubmit(){
+
+    /*    
+    jquery  dom操作
+
+      <input type="text" id="username" />
+      let usernameDom:any=document.getElementById('username');
+      console.log(usernameDom.value);    
+    */
+
+
+    console.log(this.peopleInfo);
+
+
   }
 
 }
